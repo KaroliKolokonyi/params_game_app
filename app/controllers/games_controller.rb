c85692a2ed1d55d8b812110e786dccf981fc10a8
Karoli Kolokonyi <karoli@kepler.org>
2017-01-19 18:01:06 +0200

    final application

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,41 @@
 class GamesController < ApplicationController
  
+
  def games_view 
 
     @output = params[:name]
-    puts @output
 
-    @name=@output[0]
+    if @output.class == String
 
-    if @name == "A"
-      @response ="Hey, your name starts with the first letter of alphabet"
-    end 
+        @name= @output[0]
+        @output = " #{@output.upcase}"
+        puts @output
+
+        if @name == "A"
+         @response ="Hey, your name starts with the first letter of alphabet"
+        
+        end 
+
+      else @name = (" ")
+           @output = "Please enter your name in the url starting with a question mark like this; ?name=yourname"
+   end 
 
     render "view_info.html.erb"
   end
 
+  def guess_number
+    @number = params[:number]
+    @random_number = @number.to_i 
+
+    if@random_number > 35
+        @answer = "Guess lower"
+
+     elsif  @random_number < 35
+         @answer = "Guess Higher"
+
+      elsif @random_number == 35
+        @answer = "Congraculations you have Won"
+      end 
+      render "guess_number.html"
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
- get "/view_info", to: "games#games_view"
+ get "/view_info", to: 'games#games_view'
+ get "/guess_numbers", to: 'games#guess_number'
 end
