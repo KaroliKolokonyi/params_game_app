c7beddc8174f77bc48a46502555babedf3488c09
Karoli Kolokonyi <karoli@kepler.org>
2017-01-19 15:59:57 +0200

    app developme question two

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+ 
+ def games_view 
+
+    @output = params[:name]
+    puts @output
+
+    @name=@output[0]
+
+    if @name == "A"
+      @response ="Hey, your name starts with the first letter of alphabet"
+    end 
+
+    render "view_info.html.erb"
+  end
+
 end
